18c14b0ee55ea8969a084a3032fd7475f7db53a4
Johannes Kamprad <archlinux@kamprad.net>
2025-05-20 22:35:50 +0200

    Update ISO_mode.bash

--- ISO_mode.bash
+++ ISO_mode.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 # use this raw url and add it in welcome plus adding packages.list in use_pkglist.txt in liveiser home path.
 
-
-    local -r username="$1"              # new user you created for the target
+    local -r username="$1"
     git clone https://github.com/killajoe/joe_sway.git
     cd joe_sway
     cp -a fastfetch i3status mako rofi sway /home/$username/.config/
