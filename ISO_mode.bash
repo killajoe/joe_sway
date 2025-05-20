34265d5eaa6b4c0450090ba2adb6cad2b19dff5f
Johannes Kamprad <archlinux@kamprad.net>
2025-05-20 23:22:02 +0200

    Update ISO_mode.bash

--- ISO_mode.bash
+++ ISO_mode.bash
@@ -6,7 +6,11 @@
     git clone https://github.com/killajoe/joe_sway.git
     cd joe_sway
     mkdir /home/$username/.config
-    cp -a fastfetch i3status mako rofi sway /home/$username/.config/
+    cp -a fastfetch  /home/$username/.config/
+    cp -a i3status  /home/$username/.config/
+    cp -a mako /home/$username/.config/
+    cp -a rofi /home/$username/.config/
+    cp -a sway /home/$username/.config/
     chown -R $username:$username /home/$username/.config
     chmod +x /home/$username/.config/sway/scripts/*
     systemctl enable ly
